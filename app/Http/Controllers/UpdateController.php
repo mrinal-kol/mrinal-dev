a0033434ab2711817301e12d04eec5fa1e5e3d02
dev <ronojit.dev20151@gmail.com>
2025-08-05 21:16:34 +0530

    add update controller1

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -2,12 +2,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student_details;
 class UpdateController extends Controller
 {
     public function index()
     {
+        $details =Student_details::All();
+        echo "<pre>";
+        print_r($details->toArray());
         //return "i love you chirag";
-        $surce='mrinal';
-         return view('home', compact('surce'));
+        //$surce='mrinal';
+        // return view('home', compact('surce'));
     }
 }
