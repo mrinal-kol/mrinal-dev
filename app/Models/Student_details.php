3e57fa11fa1ff20546c9f550c671b2d1dcea7a56
dev <ronojit.dev20151@gmail.com>
2025-08-08 09:24:56 +0530

    add login page issue resolve

--- app/Models/Student_details.php
+++ app/Models/Student_details.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-//use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Student_details extends Model
+class Student_details extends Authenticatable
 {
+    use Notifiable;
+
     protected $table = 'student_details';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'status',
+        // Add other fillable fields if needed
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
