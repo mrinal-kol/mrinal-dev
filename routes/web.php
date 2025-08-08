cb3f7e535ab631a025e468ea3989da833749d6ba
dev <ronojit.dev20151@gmail.com>
2025-08-08 18:07:36 +0530

    add middlewaew

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\UpdateController;
 /*
 */
+// Public routes (no middleware)
+Route::get('/', [UpdateController::class, 'index'])->name('home');
+Route::get('/login', [UpdateController::class, 'index'])->name('login');
+Route::post('/login', [UpdateController::class, 'login'])->name('login.submit');
 
+// Protected routes
+Route::middleware(['auth', 'check.student.status'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/update', [UpdateController::class, 'index'])->name('update');
+    Route::post('/submit', [FormController::class, 'submitForm'])->name('submitForm');
+
+    Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
+    Route::get('/services', [HomeController::class, 'services'])->name('services');
+    Route::get('/Portfolio', [HomeController::class, 'Portfolio'])->name('Portfolio');
+    Route::get('/contactus', [HomeController::class, 'contactus'])->name('contactus');
+    Route::get('/fetchData/{id}', [HomeController::class, 'fetchData'])->name('fetchData');
+});
+
+/*
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\UpdateController;
@@ -20,5 +42,6 @@ Route::get('/contactus', [HomeController::class, 'contactus'])->name('contactus'
 Route::get('/fetchData/{id}', [HomeController::class, 'fetchData'])->name('fetchData');
 Route::get('/login', [UpdateController::class, 'index'])->name('login');
 Route::post('/login', [UpdateController::class, 'login'])->name('login.submit');
+*/
 // use App\Http\Controllers\SchoolFormController;
 // Route::get('/school_form', [SchoolFormController::class, 'school_form']);
